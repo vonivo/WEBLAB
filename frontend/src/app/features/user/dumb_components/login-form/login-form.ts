51eb77d2e27e4fe797c635b8c0b35c9aa6dd9c45
Ivo von Rickenbach <ivo.vonrickenbach@gmail.com>
2026-09-02 13:58:30 +0000

    Add Custom color palette

--- frontend/src/app/features/user/dumb_components/login-form/login-form.ts
+++ frontend/src/app/features/user/dumb_components/login-form/login-form.ts
@@ -1,32 +1,10 @@
 import { Component } from '@angular/core';
-import {
-  MatCard,
-  MatCardActions,
-  MatCardContent,
-  MatCardHeader,
-  MatCardImage,
-  MatCardSubtitle,
-  MatCardTitle,
-} from '@angular/material/card';
 import { MatButton } from '@angular/material/button';
 import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
 import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
-  imports: [
-    MatCard,
-    MatCardHeader,
-    MatCardTitle,
-    MatCardSubtitle,
-    MatCardContent,
-    MatCardActions,
-    MatButton,
-    MatCardImage,
-    MatFormField,
-    MatLabel,
-    TranslatePipe,
-    MatInput,
-  ],
+  imports: [MatButton, MatFormField, MatLabel, TranslatePipe, MatInput],
   selector: 'app-login-form',
   styleUrl: './login-form.css',
   templateUrl: './login-form.html',
